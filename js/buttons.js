8b3a99be6b95706ec03290c0d84e471498c36fc2
bbrdak <112489999+bbrdak@users.noreply.github.com>
2022-12-06 00:01:37 -0500

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,20 @@
+function brass() {
+    document.getElementById("sectionInfo").innerHTML = "The Brass Section is made up of Trumpets, Mellophones, Baritones, Euphoniums, and Tubas. With about 80 members, it is the largest in the Corps!";
+    document.getElementById("sectionImage").src = "images/22Me.jpg"
+}
+
+function battery() {
+    document.getElementById("sectionInfo").innerHTML = "The Battery is made up of Snare, Tenor, and Bass Drums. It is the toughest section to earn a spot in, with upwards of 100 people auditioning for one spot!";
+    document.getElementById("sectionImage").src = "images/22Drumline.jpg"
+}
+
+function pit() {
+    document.getElementById("sectionInfo").innerHTML = "The Pit is the most diverse in instrumentation, and includes instruments such as the marimba, vibraphone, rack, drumset, cymbals, timpani, and more!";
+    document.getElementById("sectionImage").src = "images/BACPit.jpg"
+}
+
+
+function guard() {
+    document.getElementById("sectionInfo").innerHTML = "The Colorguard uses various forms of equipment, including Flags, Rifles, and Sabres. They do various tricks with these tools throughout the show!";
+    document.getElementById("sectionImage").src = "images/SOSGuard.jpg"
+}
---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+//use document.links to look at the different links for the page
+let index = 0;
+for(let i = 0; i < document.links.length; ++i) {
+    if(document.links[i].href == document.URL) {
+        index = i;
+        break;
+    }
+}
+document.links[index].classList.add("selected");
+document.links[index + 3].classList.add("selected");
