da1041b8c86558a32f9a30e86402b2172021dd20
bbrdak <112489999+bbrdak@users.noreply.github.com>
2022-12-06 16:37:21 -0500

    Add files via upload

--- js/buttons.js
+++ js/buttons.js
@@ -1,20 +1,20 @@
 function brass() {
-    document.getElementById("sectionInfo").innerHTML = "The Brass Section is made up of Trumpets, Mellophones, Baritones, Euphoniums, and Tubas. With about 80 members, it is the largest in the Corps!";
-    document.getElementById("sectionImage").src = "images/22Me.jpg"
+    document.getElementById("section-info").innerHTML = "The Brass Section is made up of Trumpets, Mellophones, Baritones, Euphoniums, and Tubas. With about 80 members, it is the largest in the Corps!";
+    document.getElementById("section-image").src = "images/22Me.jpg"
 }
 
 function battery() {
-    document.getElementById("sectionInfo").innerHTML = "The Battery is made up of Snare, Tenor, and Bass Drums. It is the toughest section to earn a spot in, with upwards of 100 people auditioning for one spot!";
-    document.getElementById("sectionImage").src = "images/22Drumline.jpg"
+    document.getElementById("section-info").innerHTML = "The Battery is made up of Snare, Tenor, and Bass Drums. It is the toughest section to earn a spot in, with upwards of 100 people auditioning for one spot!";
+    document.getElementById("section-image").src = "images/22Drumline.jpg"
 }
 
 function pit() {
-    document.getElementById("sectionInfo").innerHTML = "The Pit is the most diverse in instrumentation, and includes instruments such as the marimba, vibraphone, rack, drumset, cymbals, timpani, and more!";
-    document.getElementById("sectionImage").src = "images/BACPit.jpg"
+    document.getElementById("section-info").innerHTML = "The Pit is the most diverse in instrumentation, and includes instruments such as the marimba, vibraphone, rack, drumset, cymbals, timpani, and more!";
+    document.getElementById("section-image").src = "images/BACPit.jpg"
 }
 
 
 function guard() {
-    document.getElementById("sectionInfo").innerHTML = "The Colorguard uses various forms of equipment, including Flags, Rifles, and Sabres. They do various tricks with these tools throughout the show!";
-    document.getElementById("sectionImage").src = "images/SOSGuard.jpg"
+    document.getElementById("section-info").innerHTML = "The Colorguard uses various forms of equipment, including Flags, Rifles, and Sabres. They do various tricks with these tools throughout the show!";
+    document.getElementById("section-image").src = "images/SOSGuard.jpg"
 }
--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 //use document.links to look at the different links for the page
 let index = 0;
 for(let i = 0; i < document.links.length; ++i) {
-    if(document.links[i].href == document.URL) {
+    if(document.links[i].href == document.URL || document.URL == 'https://bbrdak.github.io/339-Final-Project/') {
         index = i;
         break;
     }
